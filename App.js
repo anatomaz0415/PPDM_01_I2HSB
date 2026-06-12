10aa5c6d4663c0a716f1213ceea72ebdd6957be2
Ana Tomaz <60885268+anatomaz0415@users.noreply.github.com>
2026-06-12 15:12:49 -0300

    Refactor import statement for TelaTaverna

--- App.js
+++ App.js
@@ -2,15 +2,15 @@
 libs: 
 npm i react-native-screens react-native-safe-area-context react-native-gesture-handler
 npm install @react-navigation/native @react-navigation/stack 
+essa não é uma todo list comum, é uma lista de tarefas gamificada
+você faz 'missões' e cada missão tem uma pontuação 'XP'
  */
-
 import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Importando as telas que vamos criar
-import TelaTaverna from './src/screens/TelaTaverna';
+import { TelaTaverna } from './src/screens/TelaTaverna';
 import { TelaPergaminho } from './src/screens/TelaPergaminho';
 
 const Stack = createStackNavigator();
@@ -19,13 +19,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Taverna">
-        {/* Registrando a Tela 1 */}
         <Stack.Screen 
           name="Taverna" 
           component={TelaTaverna} 
-          options={{ title: 'Diário de Missões' }} // Título no cabeçalho
+          options={{ title: 'Diário de Missões' }} // Título no cabeçalho -> action bar
         />
-        {/* Registrando a Tela 2 */}
         <Stack.Screen 
           name="Pergaminho" 
           component={TelaPergaminho} 
